a99d4e2d06794e76a19f053e278e9c040e209ab7
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-09 18:50:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mobileapps_mentalhealthapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.mobileapps_mentalhealthapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapps_mentalhealthapp.ui.theme.MobileApps_MentalHealthAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileApps_MentalHealthAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobileApps_MentalHealthAppTheme {
+        Greeting("Android")
+    }
+}
