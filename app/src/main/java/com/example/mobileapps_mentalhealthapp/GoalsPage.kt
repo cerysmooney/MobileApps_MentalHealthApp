540d423d591a80aac6964ef8e20271606df1fdee
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-10 10:36:59 +0000

    Added Goals Page and synced button to switch between pages

--- app/src/main/java/com/example/mobileapps_mentalhealthapp/GoalsPage.kt
+++ app/src/main/java/com/example/mobileapps_mentalhealthapp/GoalsPage.kt
@@ -1,8 +1,6 @@
 package com.example.mobileapps_mentalhealthapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobileapps_mentalhealthapp.ui.theme.MobileApps_MentalHealthAppTheme
 
-class HomePage : ComponentActivity() {
+class GoalsPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,31 +22,25 @@ class HomePage : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting3("Android")
+                    Greeting4("Android")
                 }
             }
-        }
-        setContentView(R.layout.home_page)
-        val homePageButtonGoals = findViewById<Button>(R.id.button)
-        homePageButtonGoals.setOnClickListener{
-            val Intent = Intent(this,HomePage::class.java)
-            startActivity(Intent)
         }
     }
 }
 
 @Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
+fun Greeting4(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! This is the Home Page",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
+fun GreetingPreview4() {
     MobileApps_MentalHealthAppTheme {
-        Greeting3("Olivia")
+        Greeting4("Android")
     }
 }
