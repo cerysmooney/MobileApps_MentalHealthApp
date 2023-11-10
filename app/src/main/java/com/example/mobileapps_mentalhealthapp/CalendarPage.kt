1090a3faa5124494d21533d0a0c212437e6be10d
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-11-10 11:51:51 +0000

    Created Calendar Page

--- app/src/main/java/com/example/mobileapps_mentalhealthapp/CalendarPage.kt
+++ app/src/main/java/com/example/mobileapps_mentalhealthapp/CalendarPage.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobileapps_mentalhealthapp.ui.theme.MobileApps_MentalHealthAppTheme
 
-class HomePage : ComponentActivity() {
+class CalendarPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,37 +24,25 @@ class HomePage : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting3("Android")
+                    Greeting5("Android")
                 }
             }
         }
-        setContentView(R.layout.home_page)
-        val goalsPageButton = findViewById<Button>(R.id.button3)
-        goalsPageButton.setOnClickListener{
-            val Intent = Intent(this,GoalsPage::class.java)
-            startActivity(Intent)
-        }
-        val calendarPageButton = findViewById<Button>(R.id.button5)
-        calendarPageButton.setOnClickListener{
-            val Intent = Intent(this,GoalsPage::class.java)
-            startActivity(Intent)
-        }
-
     }
 }
 
 @Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
+fun Greeting5(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! This is the Home Page",
+        text = "Hello $name! calendar",
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
+fun GreetingPreview5() {
     MobileApps_MentalHealthAppTheme {
-        Greeting3("Olivia")
+        Greeting5("Android")
     }
 }
